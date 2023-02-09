301d1c7b7a3741a4279b4e810252f85ec9ea9245
plabs2 <121339906+plabs2@users.noreply.github.com>
2023-02-08 19:06:08 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const prompt = require("prompt-sync")()
-const data = require("./platforms.js")
+const data = require("./platforms.js").sort((a, b) => a.name.localeCompare(b.name))
 
 for (const [index, platform] of Object.entries(data)) {
   console.log(`[${index}] ${platform.name}`)
